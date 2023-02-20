f1c86d9e380dde6f335c04113f44341c78e1039c
kihiuFrank <franklinekihiu@gmail.com>
2023-02-20 14:35:41 +0300

    challenge 4

---
+++ Challenge_3/Basics.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day4 {
+    function evaluate(int a, int b) public pure returns (int) {
+        int result = ((a + b) - (a - b));
+        return result;
+    }
+}
