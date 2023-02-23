95e1252e436d8ce12c796fbc9f065e99c50bcfe6
kihiuFrank <franklinekihiu@gmail.com>
2023-02-23 14:21:58 +0300

    power

---
+++ Challenge_9/Power.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day4 {
+    function power(uint x, uint y) public pure returns (uint) {
+        return x ** y;
+
+        //or
+        /* while (y > 0) {
+            return x ** y;
+        } */
+    }
+}
